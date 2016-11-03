8ce1ab00a5d35d8d4e93c25f24d95b4e0b17c27b
Mahmoud Zakaria <m.zakria90@gmail.com>
2016-11-03 21:04:53 +0200

    update the workflow

--- src/js/script.js
+++ src/js/script.js
@@ -3,12 +3,6 @@
 var gv = require("./give.js")
 
 
-//Calling all functions on load events
-window.addEventListener('load',function(){
-	gv.direction();
-	gv.burger();
-	gv.sliderInit();
-	gv.dropdown('click');
-})
+//Custom
 
 
