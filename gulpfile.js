bfdbfa0b25e4a68e171fcd5718d9c70a2b6585a2
MAZA2 <mahmoud.zakria@mondiamedia.com>
2017-03-05 19:38:39 +0200

    last commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,45 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var connect = require('gulp-connect');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-
-
 
 //sass
 gulp.task('sass', function () {
    sass('./src/sass/en/*.sass',{style:'expanded'})
+   .on('error', sass.logError)
+    .pipe(connect.reload())
+    .pipe(gulp.dest('./public/css'));
+   sass('./src/sass/en/shortSnippets.sass',{style:'expanded'})
     .on('error', sass.logError)
     .pipe(connect.reload())
     .pipe(gulp.dest('./public/css'));
 });
 
-gulp.task('sass-ar', function () {   
-   sass('./src/sass/ar/*.sass',{style:'expanded'})
-    .on('error', sass.logError)
-    .pipe(connect.reload())
-    .pipe(gulp.dest('./public/css'));
-});
 
 //watch 
 gulp.task('watch',function(){
-	gulp.watch('./src/sass/en/*.sass',['sass'])
- 	gulp.watch('./src/sass/ar/*.sass',['sass-ar'])
-	gulp.watch('./public/**/*.html',['html'])
-	gulp.watch('./src/js/script.js',['browserify'])
+    gulp.watch('./src/sass/en/*.sass',['sass'])
+    gulp.watch('./public/**/*.html',['html'])
+    gulp.watch('./src/js/*.js',['compress'])
 })
 
 
 //html
 gulp.task('html', function() {
-	gulp.src('./public/**/*.html')
-		.pipe(connect.reload());
+    gulp.src('./public/**/*.html')
+        .pipe(connect.reload());
 
 })
 
-//js
-gulp.task('browserify', function() {
-    return browserify('./src/js/script.js')
-        .bundle()
-        //Pass desired output filename to vinyl-source-stream
-        .pipe(source('script.js'))
-        // Start piping stream to tasks!
-        .pipe(gulp.dest('./public/js/'))
-        .pipe(connect.reload());
-});
+
+
 
 //connect 
 gulp.task('server',function(){
-	connect.server({
-		root: 'public',
-		livereload: true
-	})
+    connect.server({
+        root: 'public',
+        livereload: true
+    })
 })
 
 //default
-gulp.task('default',['watch','server','browserify'])
+gulp.task('default',['watch','server'])
